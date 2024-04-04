fe0b3eef811e984ee54e4d43215da386dd19bc23
Kathryn <kathryn@KathryndeMacBook-Pro.local>
2024-04-03 18:15:16 -0700

    first commit

---
+++ javascriot.js
@@ -0,0 +1,14 @@
+// Simulating no internet connection
+function search() {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('popupbut').style.display = 'block';
+}
+// Close the popup
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('popupbut').style.display = 'none';
+}
+
+// function submitOrder() {
+    // alert("Your order is placed");
+// }
